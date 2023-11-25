eba733ed8bfd2ec55889c135ae8a50dfba8d0810
Mahathir-ali <mahathirsabri77@gmail.com>
2023-11-26 01:57:54 +1100

    app.ts modified

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import { UserRoutes } from "./modules/user.route";
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+//application routes
+app.use("/api/users", UserRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello From Hand Written server");
